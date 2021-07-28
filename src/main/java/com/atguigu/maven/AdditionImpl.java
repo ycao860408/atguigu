ead13de2cd7bb92aa304e47395101eafee92bcc6
ycao860408 <39286519+ycao860408@users.noreply.github.com>
2021-07-27 22:19:36 -0400

    Update AdditionImpl.java

--- src/main/java/com/atguigu/maven/AdditionImpl.java
+++ src/main/java/com/atguigu/maven/AdditionImpl.java
@@ -1,7 +1,12 @@
 package com.atguigu.maven;
 
 public class AdditionImpl {
+    
+    private Integer a = 10;
+    
     public static void main(String[] args) {
         System.out.println("New additional Algorithms!");
     }
+    
+    
 }
